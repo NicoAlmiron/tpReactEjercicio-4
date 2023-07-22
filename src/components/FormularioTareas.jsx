49e9c2960801f632f9b10c27e3bbf00ae0812e7e
Nico Almiron <almiron.nicolas16@gmail.com>
2023-07-22 20:25:24 -0300

    se agrego lo que accidentalmente se subio a la rama principal

--- src/components/FormularioTareas.jsx
+++ src/components/FormularioTareas.jsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 import ListaTareas from "./ListaTareas";
 
 const FormularioTareas = () => {
+  const [tarea, setTarea] = useState("");
+  const [listaTareas, setlistaTareas] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setlistaTareas([...listaTareas, tarea]);
+    setTarea("");
+  };
+
   return (
-    <div>
-      <Form>
+    <>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="d-flex my-3" controlId="tarea">
           <Form.Control
             type="text"
             placeholder="Ingrese aqui su tarea"
             autoFocus
+            value={tarea}
+            onChange={(e) => setTarea(e.target.value)}
           ></Form.Control>
           <Button variant="success" type="submit">
             Agregar
@@ -18,7 +29,7 @@ const FormularioTareas = () => {
         </Form.Group>
         <ListaTareas></ListaTareas>
       </Form>
-    </div>
+    </>
   );
 };
 
